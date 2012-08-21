60e5d689258d467cfa9bbdcefd553980cf46cb05
Jay Zeschin <jay.zeschin@modeset.com>
2012-08-21 16:50:33 -0600

    Add a ShiftIt recipe

---
+++ shiftit/recipes/default.rb
@@ -0,0 +1,8 @@
+application 'ShiftIt' do
+  source 'https://github.com/downloads/fikovnik/ShiftIt/ShiftIt-1.5.zip'
+end
+
+execute "Start ShiftIt on login" do
+  command "defaults write loginwindow AutoLaunchedApplicationDictionary -array-add '{ \"Path\" = \"/Applications/ShiftIt.app\"; \"Hide\" = 0; }'"
+  user ENV['SUDO_USER'] || ENV['USER']
+end
