f407290e255038c5f2ef27d709f5d3221bffb364
Jay Zeschin <jay.zeschin@modeset.com>
2012-08-21 16:50:17 -0600

    Start Hiss on login

--- hiss/recipes/default.rb
+++ hiss/recipes/default.rb
@@ -1,3 +1,8 @@
 application 'Hiss' do
   source 'http://collect3.com.au/hiss/Hiss.zip'
 end
+
+execute "Start Hiss on login" do
+  command "defaults write loginwindow AutoLaunchedApplicationDictionary -array-add '{ \"Path\" = \"/Applications/Hiss.app\"; \"Hide\" = 0; }'"
+  user ENV['SUDO_USER'] || ENV['USER']
+end
