94a2ea83b061a0466cb46ad065ea7296810b1c93
Jay Zeschin <jay.zeschin@modeset.com>
2012-08-22 14:42:34 -0600

    Update dropbox recipe

---
+++ dropbox/recipes/default.rb
@@ -0,0 +1,3 @@
+application 'Dropbox' do
+  source 'https://ddr3luum8vl5r.cloudfront.net/Dropbox%201.4.12.dmg'
+end
--- dropbox_client/recipes/default.rb
+++
@@ -1,3 +0,0 @@
-application 'Dropbox' do
-  source 'http://cdn.dropbox.com/Dropbox%201.1.35.dmg'
-end
