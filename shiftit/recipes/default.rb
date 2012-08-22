d01ea1c643fb916be56172be11f2ec55bdda6605
Jay Zeschin <jay.zeschin@modeset.com>
2012-08-22 14:42:23 -0600

    ShiftIt recipe should enable assistive device support

--- shiftit/recipes/default.rb
+++ shiftit/recipes/default.rb
@@ -2,6 +2,13 @@ application 'ShiftIt' do
   source 'http://cloud.github.com/downloads/fikovnik/ShiftIt/ShiftIt-1.5.zip'
 end
 
+# Enable assistive device access
+file "/private/var/db/.AccessibilityAPIEnabled" do
+  action :create_if_missing
+  content "a"
+  mode "0444"
+end
+
 execute "Start ShiftIt on login" do
   command "defaults write loginwindow AutoLaunchedApplicationDictionary -array-add '{ \"Path\" = \"/Applications/ShiftIt.app\"; \"Hide\" = 0; }'"
   user ENV['SUDO_USER'] || ENV['USER']
