949f4173108a388632f92b28f7534b83c484228f
Jay Zeschin <jay.zeschin@modeset.com>
2012-08-22 15:05:17 -0600

    Add a recipe for Postgres.app

---
+++ postgres.app/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Jay Zeschin"
+maintainer_email "jay.zeschin@modeset.com"
+license          "MIT"
+description      "Installs the Postgres.app frontend"
+version          "0.1.0"
+
+supports "mac_os_x"
---
+++ postgres.app/recipes/default.rb
@@ -0,0 +1,3 @@
+application 'Postgres' do
+  source 'https://mesmerize.s3.amazonaws.com/Postgres/Postgres-11.zip'
+end
