384554f1b658c9a9a35d682f462430e2b62069c0
Jay Zeschin <jay.zeschin@modeset.com>
2012-08-22 18:26:01 -0600

    Add a recipe for GrandPerspective

---
+++ grandperspective/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Jay Zeschin"
+maintainer_email "jay.zeschin@modeset.com"
+license          "MIT"
+description      "Installs the GrandPerspective disk usage app"
+version          "0.1.0"
+
+supports "mac_os_x"
---
+++ grandperspective/recipes/default.rb
@@ -0,0 +1,3 @@
+application 'GrandPerspective' do
+  source 'http://superb-sea2.dl.sourceforge.net/project/grandperspectiv/grandperspective/1.5.0/GrandPerspective-1_5_0.dmg'
+end
